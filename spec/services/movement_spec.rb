da95883bfecdf12d5415c7ec335dc9ec49aa2d5f
Thiago dos Anjos <thiagonanjos@gmail.com>
2024-03-18 11:18:18 -0300

    refactor(Movement): Movement Spec

--- spec/services/movement_spec.rb
+++ spec/services/movement_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe 'Movement', :type => :service do
-  let(:file_path) { './spec/fixtures/files/input.txt' }
-  let(:file) { File.open(file_path) }
-  let(:exploration_data) { Parser.call(file) }
+  let(:input_file_path) { './spec/fixtures/files/input.txt' }
+  let(:input_file) { File.open(input_file_path) }
+  let(:exploration_data) { Parser.call(input_file) }
   let(:instructions) { exploration_data[:instructions].first }
   let(:starting_position) { instructions[:starting_position].split(' ') }
   let(:commands) { instructions[:commands].chars }
